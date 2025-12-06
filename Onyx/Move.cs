36ec6b33676d8194e276fa07276727ef54ff0b0d
Jastaw21 <jacksweddell@outlook.com>
2025-12-06 21:25:31 +0000

    Add move struct

---
+++ Onyx/Move.cs
@@ -0,0 +1,19 @@
+﻿namespace Onyx;
+
+public struct Move(Piece piece, Square from, Square to)
+{
+    public Square From { get; } = from;
+    public Square To { get; } = to;
+    public Piece Piece1 { get; } = piece;
+
+    public Piece? CapturedPiece = null;
+    public Piece? PromotedPiece = null;
+
+    public string Notation
+    {
+        get
+        {
+            return From.Notation + To.Notation;
+        }
+    }
+}
