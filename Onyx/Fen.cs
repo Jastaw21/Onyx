8e285d5b28440f3082ded945ec3c238208382175
Jastaw21 <jacksweddell@outlook.com>
2025-12-06 21:25:39 +0000

    Fen wrapper

---
+++ Onyx/Fen.cs
@@ -0,0 +1,8 @@
+﻿namespace Onyx;
+using FenString = string;
+public static class Fen
+{
+    
+    public static readonly FenString DefaultFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    
+}
