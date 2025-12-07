64c9143c0d1087f52c07d6e7191031dac7acfdaf
Jack Weddell <jacksweddell@outlook.com>
2025-12-07 18:29:14 +0000

    Add magic bitboard gen

---
+++ MagicBitboardGenerator/MaskGenerator.cs
@@ -0,0 +1,70 @@
+﻿using Onyx;
+
+namespace MagicBitboardGenerator;
+
+file struct MoveDir(int deltaRank, int deltaFile)
+{
+    public readonly int DeltaRank = deltaRank;
+    public readonly int DeltaFile = deltaFile;
+
+    public static MoveDir[] All()
+    {
+        return
+        [
+            new MoveDir(1, 1),
+            new MoveDir(1, -1),
+            new MoveDir(-1, -1),
+            new MoveDir(-1, 1)
+        ];
+    }
+}
+
+public static class MaskGenerator
+{
+    public static ulong GenerateStraightMoves(int square)
+    {
+        var result = 0ul;
+
+        var rank = RankAndFileHelpers.RankIndex(square);
+        var file = RankAndFileHelpers.FileIndex(square);
+
+        // ignore the borders
+        for (var r = 1; r < 7; r++)
+        {
+            if (r != rank)
+                result |= 1ul << RankAndFileHelpers.SquareIndex(r, file);
+        }
+
+        for (var f = 1; f < 7; f++)
+        {
+            if (f != file)
+                result |= 1ul << RankAndFileHelpers.SquareIndex(rank, f);
+        }
+
+        return result;
+    }
+
+    public static ulong GenerateDiagonalMoves(int square)
+    {
+        var result = 0ul;
+
+        var rank = RankAndFileHelpers.RankIndex(square);
+        var file = RankAndFileHelpers.FileIndex(square);
+
+        foreach (var direction in MoveDir.All())
+        {
+            var targetRank = rank + direction.DeltaRank;
+            var targetFile = file + direction.DeltaFile;
+
+            while (targetRank is > 0 and < 7 && targetFile is > 0 and < 7)
+            {
+                result |= 1ul << RankAndFileHelpers.SquareIndex(targetRank, targetFile);
+
+                targetFile += direction.DeltaFile;
+                targetRank += direction.DeltaRank;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ OnyxTests/MagicBitboardTests.cs
@@ -0,0 +1,28 @@
+﻿using MagicBitboardGenerator;
+
+namespace OnyxTests;
+
+public class MagicBitboardTests
+{
+    [Test]
+    public void MaskGenTests()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(MaskGenerator.GenerateStraightMoves(0), Is.EqualTo(0x101010101017e));
+            Assert.That(MaskGenerator.GenerateStraightMoves(8), Is.EqualTo(0x1010101017e00));
+            Assert.That(MaskGenerator.GenerateStraightMoves(16), Is.EqualTo(0x10101017e0100));
+            Assert.That(MaskGenerator.GenerateStraightMoves(55), Is.EqualTo(0x7e808080808000));
+        });
+        
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(MaskGenerator.GenerateDiagonalMoves(0), Is.EqualTo(0x40201008040200));
+            Assert.That(MaskGenerator.GenerateDiagonalMoves(8), Is.EqualTo(0x20100804020000));
+            Assert.That(MaskGenerator.GenerateDiagonalMoves(16), Is.EqualTo(0x10080402000200));
+            Assert.That(MaskGenerator.GenerateDiagonalMoves(55), Is.EqualTo(0x402010080400));
+        });
+    }
+    
+}
