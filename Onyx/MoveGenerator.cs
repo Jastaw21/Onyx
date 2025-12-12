d5015c17e4e17c30e66126c56c6faff11a2c54ba
Jastaw21 <jacksweddell@outlook.com>
2025-12-12 19:10:15 +0000

    Add castling move gen

--- Onyx/MoveGenerator.cs
+++ Onyx/MoveGenerator.cs
@@ -5,6 +5,60 @@ public static class MoveGenerator
     public static List<Move> GetMoves(Piece piece, Square square, ref Board board)
     {
         var moveList = new List<Move>();
+        GenerateBasicMoves(piece, square, ref board, moveList);
+        GenerateCastlingMoves(piece, square, ref board, moveList);
+
+        return moveList;
+    }
+
+    private static void GenerateCastlingMoves(Piece piece, Square square, ref Board board, List<Move> moveList)
+    {
+        if (piece.Type != PieceType.King)
+            return;
+
+        if (board.CastlingRights == 0)
+            return;
+
+        var result = 0ul;
+        bool kingSideEmpty;
+        bool queenSideEmpty;
+        switch (piece.Colour)
+        {
+            case Colour.White:
+                if (square.SquareIndex != BoardConstants.E1) return;
+                kingSideEmpty = (BoardConstants.WhiteKingSideCastlingSquares & board.Bitboards.Occupancy()) == 0;
+                queenSideEmpty = (BoardConstants.WhiteKingSideCastlingSquares & board.Bitboards.Occupancy()) == 0;
+                if (kingSideEmpty && ((board.CastlingRights & BoardConstants.WhiteKingsideCastlingFlag) > 0)) 
+                    result |= 1ul << BoardConstants.G1;
+                
+                if (queenSideEmpty && ((board.CastlingRights & BoardConstants.WhiteQueensideCastlingFlag) > 0)) 
+                    result |= 1ul << BoardConstants.C1;
+                
+                break;
+
+            case Colour.Black:
+                if (square.SquareIndex != BoardConstants.E8) return;
+                kingSideEmpty = (BoardConstants.BlackKingSideCastlingSquares & board.Bitboards.Occupancy()) == 0;
+                queenSideEmpty = (BoardConstants.BlackKingSideCastlingSquares & board.Bitboards.Occupancy()) == 0;
+                
+                if (kingSideEmpty && ((board.CastlingRights & BoardConstants.BlackKingsideCastlingFlag) > 0)) 
+                    result |= 1ul << BoardConstants.G8;
+                
+                if (queenSideEmpty && ((board.CastlingRights & BoardConstants.BlackQueensideCastlingFlag) > 0)) 
+                    result |= 1ul << BoardConstants.C8;
+                break;
+        }
+        
+        while (result > 0)
+        {
+            var lowest = ulong.TrailingZeroCount(result);
+            moveList.Add(new Move(piece, square, new Square((int)lowest)));
+            result &= result - 1;
+        }
+    }
+
+    private static void GenerateBasicMoves(Piece piece, Square square, ref Board board, List<Move> moveList)
+    {
         var moves = GetMovesUlong(piece, square, ref board);
         while (moves > 0)
         {
@@ -12,14 +66,14 @@ public static class MoveGenerator
             moveList.Add(new Move(piece, square, new Square((int)lowest)));
             moves &= moves - 1;
         }
-
-        return moveList;
     }
 
     private static ulong GetMovesUlong(Piece piece, Square square, ref Board board)
     {
         var result = 0ul;
+        var movingSideOccupancy = board.Bitboards.OccupancyByColour(piece.Colour);
         result |= MagicBitboards.GetMovesByPiece(piece, square, board.Bitboards.Occupancy());
+        result &= ~movingSideOccupancy;
         return result;
     }
 }
--- OnyxTests/MoveGenTests.cs
+++ OnyxTests/MoveGenTests.cs
@@ -1,4 +1,6 @@
-﻿using Onyx;
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
+using Onyx;
 
 namespace OnyxTests;
 
@@ -8,14 +10,74 @@ public class MoveGenTests
     public void MoveGenContainsBasicExpectedMoves()
     {
         var board = new Board(Fen.DefaultFen);
-        
+
         var aPawnMoves = MoveGenerator.GetMoves(Piece.WP, new Square(8), ref board);
-        Assert.That(aPawnMoves,Has.Member(new Move(Piece.WP,"a2a3")));
-        Assert.That(aPawnMoves,Has.Member(new Move(Piece.WP,"a2a4")));
-        
+        Assert.That(aPawnMoves, Has.Member(new Move(Piece.WP, "a2a3")));
+        Assert.That(aPawnMoves, Has.Member(new Move(Piece.WP, "a2a4")));
+
         var bKnightMoves = MoveGenerator.GetMoves(Piece.WN, new Square(1), ref board);
-        Assert.That(bKnightMoves,Has.Member(new Move(Piece.WN,"b1a3")));
-        Assert.That(bKnightMoves,Has.Member(new Move(Piece.WN,"b1c3")));
+        Assert.That(bKnightMoves, Has.Member(new Move(Piece.WN, "b1a3")));
+        Assert.That(bKnightMoves, Has.Member(new Move(Piece.WN, "b1c3")));
     }
-    
+
+    [Test]
+    public void DoesntGenerateMoveToOwn()
+    {
+        var testBoard = new Board("8/8/8/8/8/NRBBN3/PKQBN3/8 w - - 0 1");
+        List<Move> movesToTest =
+        [
+            new Move(Piece.WP, new Square(8), new Square(16)),
+            new Move(Piece.WK, new Square(9), new Square(17)),
+            new Move(Piece.WQ, new Square(10), new Square(18)),
+            new Move(Piece.WB, new Square(11), new Square(18)),
+            new Move(Piece.WB, new Square(12), new Square(18)),
+        ];
+
+        foreach (var move in movesToTest)
+        {
+            var movesByPieceBySquare = MoveGenerator.GetMoves(move.PieceMoved, move.From, ref testBoard);
+            Assert.That(movesByPieceBySquare, Does.Not.Contain(move));
+        }
+    }
+
+    [Test]
+    public void CantPassOpponents()
+    {
+        var testBoard = new Board("8/8/8/8/8/qqq1q3/8/QRB5 w - - 0 1");
+
+        List<Move> illegalMoves =
+        [
+            new Move(Piece.WQ, "a1a4"),
+            new Move(Piece.WR, "b1b4"),
+            new Move(Piece.WB, "c1f4"),
+        ];
+
+        List<Move> legalMoves =
+        [
+            new Move(Piece.WQ, "a1a3"),
+            new Move(Piece.WR, "b1b3"),
+            new Move(Piece.WB, "c1e3"),
+            new Move(Piece.WB, "c1a3"),
+        ];
+
+        foreach (var move in illegalMoves)
+        {
+            var movesByPieceBySquare = MoveGenerator.GetMoves(move.PieceMoved, move.From, ref testBoard);
+            Assert.That(movesByPieceBySquare, Does.Not.Contain(move));
+        }
+
+        foreach (var move in legalMoves)
+        {
+            var movesByPieceBySquare = MoveGenerator.GetMoves(move.PieceMoved, move.From, ref testBoard);
+            Assert.That(movesByPieceBySquare, Does.Contain(move));
+        }
+    }
+
+    [Test]
+    public void MoveGenIncludesCastling()
+    {
+        var castlingPos = new Board("rnbqkb1r/1pp2ppp/p2p1n2/4p3/2P5/2N2NP1/PP1PPPBP/1RBQK2R w Kq - 0 7");
+        var expectedCastlingMove = new Move(Piece.WK, "e1g1");
+        Assert.That(MoveGenerator.GetMoves(Piece.WK,expectedCastlingMove.From,ref castlingPos), Does.Contain(expectedCastlingMove));
+    }
 }
