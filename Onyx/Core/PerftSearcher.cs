61f6763b7412f2cdde8e3091f77951a1bd4f4e70
Jack Weddell <jacksweddell@outlook.com>
2025-12-14 21:30:30 +0000

    Start adding perft

---
+++ Onyx/Core/PerftSearcher.cs
@@ -0,0 +1,49 @@
+﻿namespace Onyx.Core;
+
+public struct PerftResults
+{
+    public PerftResults(int nodes = 0)
+    {
+        Nodes = nodes;
+    }
+
+    public int Nodes = 0;
+
+    public static PerftResults operator +(PerftResults x, PerftResults y)
+    {
+        return new PerftResults
+        {
+            Nodes = x.Nodes + y.Nodes
+        };
+    }
+}
+
+public static class PerftSearcher
+{
+    public static PerftResults GetPerftResults(Board board, int depth)
+    {
+        return ExecutePerft(board, depth);
+    }
+
+    private static PerftResults ExecutePerft(Board board, int depth)
+    {
+        if (depth == 0)
+        {
+            return new PerftResults(nodes: 1);
+        }
+
+        var results = new PerftResults();
+        var movesInPosition = MoveGenerator.GetMoves(board.TurnToMove, ref board);
+
+        foreach (var move in movesInPosition)
+        {
+            board.ApplyMove(move);
+            var childResults = ExecutePerft(board, depth - 1);
+            if (depth == 1)
+                results += childResults;
+            board.UndoMove(move);
+        }
+
+        return results;
+    }
+}
---
+++ OnyxTests/Perft.cs
@@ -0,0 +1,19 @@
+﻿using Onyx.Core;
+
+namespace OnyxTests;
+
+public class Perft
+{
+    [Test]
+    public void StartingPosDepth1()
+    {
+        var board = new Board();
+        Assert.That(PerftSearcher.GetPerftResults(board,1).Nodes,Is.EqualTo(20));
+    }
+    [Test]
+    public void StartingPosDepth2()
+    {
+        var board = new Board();
+        Assert.That(PerftSearcher.GetPerftResults(board,2).Nodes,Is.EqualTo(40));
+    }
+}
